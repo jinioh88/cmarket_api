2b6ccf472cad665ae5d99428c6ab8f90fd3c150f
jinioh <jinioh@jiniohui-MacBookPro.local>
2026-01-21 20:34:26 +0900

    이메일 인증 gmail 연결

--- service/cmarket/src/main/java/org/cmarket/cmarket/web/auth/dto/SignUpRequest.java
+++ service/cmarket/src/main/java/org/cmarket/cmarket/web/auth/dto/SignUpRequest.java
@@ -18,14 +18,14 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class SignUpRequest {
     
-    @NotBlank(message = "이메일은 필수입니다.")
+    @NotNull(message = "이메일은 필수입니다.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
     
-    @NotBlank(message = "비밀번호는 필수입니다.")
+    @NotNull(message = "비밀번호는 필수입니다.")
     private String password;
     
-    @NotBlank(message = "이름은 필수입니다.")
+    @NotNull(message = "이름은 필수입니다.")
     @Size(max = 10, message = "이름은 최대 10자까지 입력 가능합니다.")
     private String name;
     
